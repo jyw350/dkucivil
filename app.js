ec6108bf9cf006af567586a990106075339f6374
jyw350 <jangy7723@gmail.com>
2026-05-13 19:59:07 +0900

    Fix question 1-30 answer typo

--- app.js
+++ app.js
@@ -908,7 +908,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260513-10";
+    script.src = "./data/civil_quiz_dataset.js?v=20260513-11";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
