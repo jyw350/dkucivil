d7ae8327f0e8dd56faa99736e00b88236fe169e2
jyw350 <jangy7723@gmail.com>
2026-05-13 20:24:32 +0900

    Move question 1-76 memo to hint

--- app.js
+++ app.js
@@ -943,7 +943,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260513-14";
+    script.src = "./data/civil_quiz_dataset.js?v=20260513-15";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
