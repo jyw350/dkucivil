54df6850bbda299c5d0c68ab10e106c2a6942b0f
jyw350 <jangy7723@gmail.com>
2026-05-16 15:35:18 +0900

    Relax grading for tunnel ventilation question

--- app.js
+++ app.js
@@ -1132,7 +1132,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260516-1";
+    script.src = "./data/civil_quiz_dataset.js?v=20260516-2";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
