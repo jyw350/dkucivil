cf444e365de414b7e9e5654695f666210af3200e
jyw350 <jangy7723@gmail.com>
2026-05-20 16:43:20 +0900

    Refine answers 1-204 to 1-209

--- app.js
+++ app.js
@@ -1275,7 +1275,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260518-8";
+    script.src = "./data/civil_quiz_dataset.js?v=20260520-1";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
