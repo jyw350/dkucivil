c536af4787c2bfc92aa59fee66092d9031d2276c
jyw350 <jangy7723@gmail.com>
2026-05-25 13:32:05 +0900

    Update answer for question 1-158

--- app.js
+++ app.js
@@ -1420,7 +1420,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260525-8";
+    script.src = "./data/civil_quiz_dataset.js?v=20260525-9";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
