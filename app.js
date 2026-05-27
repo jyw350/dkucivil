cbca8da08dd01deca699316da56a98e624d90798
jyw350 <jangy7723@gmail.com>
2026-05-27 14:44:33 +0900

    Add keyword standards for volume 3 questions 151 to 175

--- app.js
+++ app.js
@@ -1420,7 +1420,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260527-7";
+    script.src = "./data/civil_quiz_dataset.js?v=20260527-8";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
