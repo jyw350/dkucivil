a8998734157fa9068e688973de3b1de320fbce04
jyw350 <jangy7723@gmail.com>
2026-06-01 12:34:11 +0900

    Add hint for volume 2 question 122

--- app.js
+++ app.js
@@ -1555,7 +1555,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260601-2";
+    script.src = "./data/civil_quiz_dataset.js?v=20260601-3";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
