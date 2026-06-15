8cd8d3ff71b244156b403ec9e94b69da54adcacb
jyw350 <jangy7723@gmail.com>
2026-06-15 11:56:00 +0900

    Accept jacking and shield spelling variants

--- app.js
+++ app.js
@@ -1575,7 +1575,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260615-2";
+    script.src = "./data/civil_quiz_dataset.js?v=20260615-3";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
