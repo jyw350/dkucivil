0e9cc82167da496398eabfce9c0230d22c7a64ff
jyw350 <jangy7723@gmail.com>
2026-06-17 10:08:55 +0900

    Accept Korean NDT names for question 3-158

--- app.js
+++ app.js
@@ -1575,7 +1575,7 @@ function ensureDatasetScriptLoaded() {
 
   window.__civilQuizDatasetPromise = new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./data/civil_quiz_dataset.js?v=20260616-2";
+    script.src = "./data/civil_quiz_dataset.js?v=20260617-1";
     script.async = true;
     script.onload = () => {
       if (window.CIVIL_QUIZ_DATA) {
